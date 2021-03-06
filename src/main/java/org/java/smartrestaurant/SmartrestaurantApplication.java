feb39e6829157e6b570e4f286f420c94cdea91a7
Oleg Mironenko <atek@ngs.ru>
2021-03-07 01:50:41 +0700

    init 1

---
+++ src/main/java/org/java/smartrestaurant/SmartrestaurantApplication.java
@@ -0,0 +1,13 @@
+package org.java.smartrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartrestaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartrestaurantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/java/smartrestaurant/SmartrestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package org.java.smartrestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartrestaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
